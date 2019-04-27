2cfd081afe9c601da1c725a9566e124d111412cf
Aaron Collins <aa.collins.95@gmail.com>
2019-04-26 23:00:19 -0400

    Initial commit, microreddit

---
+++ app/models/post.rb
@@ -0,0 +1,8 @@
+class Post < ApplicationRecord
+  validates :title,  presence: true, length: { maximum: 200 }
+  validates :body, presence: true
+  validates :user_id, presence: true
+
+  belongs_to :user
+  has_many :comments
+end
